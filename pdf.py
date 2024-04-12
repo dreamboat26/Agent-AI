90e86c68c95da1d72e36c44185c5b0953679d2ce
Mahule Roy <125608791+dreamboat26@users.noreply.github.com>
2024-04-13 00:04:35 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -17,7 +17,7 @@ def get_index(data, index_name):
     return index
 
 
-pdf_path = os.path.join("data", "Canada.pdf")
-canada_pdf = PDFReader().load_data(file=pdf_path)
-canada_index = get_index(canada_pdf, "canada")
-canada_engine = canada_index.as_query_engine()
+pdf_path = os.path.join("data", "India.pdf")
+india_pdf = PDFReader().load_data(file=pdf_path)
+india_index = get_index(india_pdf, "india")
+india_engine = india_index.as_query_engine()
